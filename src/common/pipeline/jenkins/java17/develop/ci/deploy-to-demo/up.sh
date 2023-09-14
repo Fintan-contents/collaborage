2357bdeafa31f6f2f155bada94e80fc4c92c3c7f
oguchi-shiori03 <oguchi.shiori@tis.co.jp>
2023-09-14 11:52:08 +0900

    パイプラインスクリプト修正

--- src/common/pipeline/jenkins/java17/develop/ci/deploy-to-demo/up.sh
+++ src/common/pipeline/jenkins/java17/develop/ci/deploy-to-demo/up.sh
@@ -4,4 +4,4 @@ set -u
 docker stop my-running-app
 docker rmi my-java-app
 docker build -t my-java-app .
-docker run --rm -d --name my-running-app -p 80:3333 my-java-app
+docker run --rm -d --name my-running-app -p 80:8080 my-java-app
